61cede9b3001267273b25c45b7e934d6ee81c80f
alexamarki <amarkin2006@gmail.com>
2023-05-02 00:00:11 +0300

    added locale to datetime

--- datetime 5/main.py
+++ datetime 5/main.py
@@ -2,7 +2,9 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 import datetime
+import locale
 
+locale.setlocale(locale.LC_ALL, 'ru_RU')
 
 def main():
     vk_session = vk_api.VkApi(
@@ -16,7 +18,8 @@ def main():
             reply = ''
             if any(word in msg.lower() for word in ["время", "дата", "число", "день"]):
                 dt_obj = datetime.datetime.utcnow() + moscow_delta
-                reply = f'Сегодня {dt_obj.strftime("%A")}, {dt_obj.strftime("%d %B, %Y")}. В Москве сейчас {dt_obj.strftime("%H час(а/ов), %M минут(а/ы), %S секунд(а/ы)")}'
+                reply = f'Сегодня {dt_obj.strftime("%A")}, {dt_obj.strftime("%d %B, %Y")}. ' \
+                        f'В Москве сейчас {dt_obj.strftime("%H час(а/ов), %M минут(а/ы), %S секунд(а/ы)")}'
             else:
                 reply = f'Спросите меня о времени, дне, дате или числе и я скажу вам их.'
             vk_sess.messages.send(user_id=event.obj.message['from_id'],
