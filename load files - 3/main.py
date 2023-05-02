25b28a085df3f18d6afe9b9e1a9501ed9a0c4892
alexamarki <amarkin2006@gmail.com>
2023-05-02 17:29:08 +0300

    photo upload added

--- load files - 3/main.py
+++ load files - 3/main.py
@@ -12,7 +12,7 @@ def auth_handler():
 def main():
     login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(login, password, auth_handler=auth_handler)
-    group_id = GROUP_ID
+    group_id = GROUP_ID #group_id - строка
     try:
         vk_session.auth(token_only=True)
     except vk_api.AuthError as error_msg:
